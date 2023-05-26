b410f4f6d86c348e3025e4c6bac7b0b63e6cf50c
maskalor <maks_inf@mail.ru>
2023-05-26 12:46:01 +1000

    fix crash

--- app/src/main/java/com/maskalor/myapplication/presentation/TaskListAdapter.kt
+++ app/src/main/java/com/maskalor/myapplication/presentation/TaskListAdapter.kt
@@ -28,7 +28,7 @@ class TaskListAdapter : ListAdapter<Task, TaskListAdapter.MyViewHolder>(MyDiffUt
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.task_layout, parent)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.task_layout, parent, false)
         return MyViewHolder(view)
     }
 
